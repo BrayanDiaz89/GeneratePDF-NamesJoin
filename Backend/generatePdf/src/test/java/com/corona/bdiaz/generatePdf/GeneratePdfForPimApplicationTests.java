23672616248107c5db9c6224ab8ffc221399f85b
Brayan Díaz <brayandiaz258f@gmail.com>
2025-10-28 16:55:24 -0500

    Subiendo el proyecto.

---
+++ Backend/generatePdf/src/test/java/com/corona/bdiaz/generatePdf/GeneratePdfForPimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corona.bdiaz.generatePdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratePdfForPimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
