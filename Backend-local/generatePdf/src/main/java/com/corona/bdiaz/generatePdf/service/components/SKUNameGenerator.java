258496d598b815a0e1e7227fa6e3b624e4a65ecb
Brayan Díaz <brayandiaz258f@gmail.com>
2025-10-30 15:12:56 -0500

    Delete line innecesary

--- Backend-local/generatePdf/src/main/java/com/corona/bdiaz/generatePdf/service/components/SKUNameGenerator.java
+++ Backend-local/generatePdf/src/main/java/com/corona/bdiaz/generatePdf/service/components/SKUNameGenerator.java
@@ -3,8 +3,6 @@ package com.corona.bdiaz.generatePdf.service.components;
 import com.corona.bdiaz.generatePdf.domain.NameDocumentGenerateRequest;
 import com.corona.bdiaz.generatePdf.domain.ResponseFromProcessedFiles;
 
-import java.util.Map;
-
 public interface SKUNameGenerator {
     ResponseFromProcessedFiles generateFiles(NameDocumentGenerateRequest request);
     String getSupportedExtension();
